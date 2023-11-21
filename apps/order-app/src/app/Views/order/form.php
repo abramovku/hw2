c1e775e1d1e3602bc3824367f61df33c052941d8
Abramov Kirill <abramovku@gmail.com>
2023-11-21 11:52:07 +0300

    form edits

--- apps/order-app/src/app/Views/order/form.php
+++ apps/order-app/src/app/Views/order/form.php
@@ -25,4 +25,18 @@
       </div>
   </form>
   </body>
+  <style>
+      let orderId = 'order';
+      const generateRandomString = (length) => {
+                                                 let result = '';
+                                                 const characters =
+                                                 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                                                 const charactersLength = characters.length;
+                                                 for (let i = 0; i < length; i++) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+      };
+      document.getElementById(orderId).value= generateRandomString(30);
+  </style>
 </html>
